1c8e9687cf461cf49f6d64d1ef8fcea5ec52dde1
Sigwon Lee <109648080+ssssigun@users.noreply.github.com>
2023-08-23 15:46:34 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,38 @@
+/*
+    1. list에서 전체 더해주고 구하기
+    2. 
+*/
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList();
+        int cnt =0;
+        int temp=0;
+        boolean run = true;
+        while(run){
+            if(cnt >= progresses.length){
+                run =false;
+                break;
+            }
+            //작업 속도 더하기
+            for(int i=cnt; i<progresses.length; i++){
+                progresses[i]+=speeds[i];
+            }
+            //작업 끝났는지 확인
+            for(int i=cnt; i<progresses.length; i++){
+                if(progresses[i]>=100){
+                    temp++;
+                }else{
+                    break;
+                }
+            }
+            // 작업이 완료됬다면 순서대로 배포
+            if(temp>0){
+                answer.add(temp);
+                cnt+=temp;
+                temp=0;
+            }
+        }
+        return answer;
+    }
+}
