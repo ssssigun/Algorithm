a5774bc069ebf0c61b749b6eb70191a3495db043
Sigwon Lee <109648080+ssssigun@users.noreply.github.com>
2023-09-22 09:36:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 172.38 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,25 @@
 /*
-    1. 완주하지 못한 사람 한명 찾기
-    2. 일단 이중 for문으로 접근해보자
+    1. 해시로 다시 풀기
+    2. 완주한 사람들을 boolean으로 관리
+    3. 없으면 false 반환하여 return
 */
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        // 완주자 관리 해시
+        Map<String,Integer> map = new HashMap<>();
+        // 완주한 사람들 체크
         for(int i=0; i<completion.length; i++){
-            if(!participant[i].equals(completion[i])){
-                answer = participant[i];
-                break;
-            }
+            map.put(completion[i],  map.getOrDefault(completion[i], 0) + 1);
         }
-        if(answer.equals("")){
-            answer = participant[participant.length-1];
+        // 참가자 중에서 완주 못한 사람 찾기
+        for(int i=0; i<participant.length; i++){
+            if(map.getOrDefault(participant[i], 0) == 0){
+                return participant[i];
+            }else{
+                map.put(participant[i], map.get(participant[i])-1);
+            }
         }
         return answer;
     }
