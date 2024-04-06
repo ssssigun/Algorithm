af0ed869c4afc093bcc4f7d49ee6ccaecf8fa041
SiGwon Lee <109648080+ssssigun@users.noreply.github.com>
2024-04-06 21:51:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.64 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<food.length; i++){
+            for(int j=0; j<food[i]/2; j++){
+                sb.append(i);
+            }
+        }
+        answer = sb.toString()+"0";
+        answer += sb.reverse().toString();
+        return answer;
+    }
+}
