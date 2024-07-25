2bfb481f4fab8a019989a93640d9682980cc6fbc
SiGwon Lee <109648080+ssssigun@users.noreply.github.com>
2024-07-25 14:39:36 +0900

    [Gold IV] Title: 운동, Time: 720 ms, Memory: 54280 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -1,7 +1,8 @@
 /*
-    1. 다익스트라로 구하기
-        - N개의 노드에 대해서 다익스트라 확인
-        - 자기 자신한테 돌아온다면 최솟값과 비교에서 저장
+    1. 플로이드 워셜로 풀기
+        - 플로이드 워셜로 거리 먼저 구하기
+        - 서로에게 가는 곳이 있으면 사이클이 있으므로 최솟값 return
+            - graph[a][b] != INF && graph[b][a] != INF
     2. 첫째 줄에 노드 V, 간선 E
         - 그 이후 출발지 a, 도착지 b, 거리 c E개 (단방향 노드)
     3. 최소 사이클의 도로 값 return
@@ -9,24 +10,9 @@
 * */
 
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
-class Node implements Comparable<Node>{
-    int n;
-    int cost;
-    Node(int n, int cost){
-        this.n = n;
-        this.cost = cost;
-    }
-    @Override
-    public int compareTo(Node o1){
-        return this.cost - o1.cost;
-    }
-}
 class Main {
-    static List<List<Node>> graph = new ArrayList<>(); // 단방향 그래프
-    static int[][] dist;
-    static int ans = Integer.MAX_VALUE; // 사이클의 최소 거리 값
     public static void main(String[] arg) throws IOException {
         // 선언
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,53 +21,41 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
-        for(int i=0; i<=V; i++){ // 노드 생성
-            graph.add(new ArrayList<>());
-        }
-        dist = new int[V+1][V+1];
+        int[][] arr = new int[V+1][V+1];
+        final int max = 100000000;
         for(int i=1; i<=V; i++){
-            Arrays.fill(dist[i],Integer.MAX_VALUE);
+            for(int j=1; j<=V; j++){
+                if(i != j){
+                    arr[i][j] = max;
+                }
+            }
         }
-
         for(int i=0; i<E; i++){ // 간선 정보 입력
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int next = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            graph.get(start).add(new Node(next, cost));
+            arr[start][next] = cost;
         }
-
-        for(int i=1; i<=V; i++) { // 전체 노드를 돌면서 사이클 확인, 사이클이면 최솟값 반환
-            dijkstra(i);
+        // 플로이드 워셜로 최소 거리 구하기
+        for(int k=1; k<=V; k++){
+            for(int i=1; i<=V; i++){
+                for(int j=1; j<=V; j++){
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
         }
+        int ans = max; // 사이클의 최소 거리 값
+        // 사이클이 있는 지 확인
         for(int i=1; i<=V; i++){
             for(int j=1; j<=V; j++){
-                if(i == j){
-                    ans = Math.min(ans, dist[i][j]);
+                if(i != j && arr[i][j] != max && arr[j][i] != max){
+                    ans = Math.min(ans, arr[i][j] + arr[j][i]);
                 }
             }
         }
         // 출력
-        bw.write((ans == Integer.MAX_VALUE ? -1 : ans)+"");
+        bw.write((ans == max ? -1 : ans)+"");
         bw.flush();
-    }
-    public static void dijkstra(int n){
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(n, 0));
-
-        while(!pq.isEmpty()){
-            Node now = pq.poll();
-            if(dist[n][now.n] < now.cost){
-                continue;
-            }
-            for(int i=0; i<graph.get(now.n).size(); i++){
-                Node next = graph.get(now.n).get(i);
-
-                if(dist[n][next.n] > now.cost + next.cost){
-                    dist[n][next.n] = now.cost + next.cost;
-                    pq.offer(new Node(next.n, dist[n][next.n]));
-                }
-            }
-        }
     }
 }
