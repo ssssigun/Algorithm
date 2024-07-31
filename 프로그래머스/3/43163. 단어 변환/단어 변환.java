b70fb8169fdd64e380b87e9526c0a335cc76fdb6
SiGwon Lee <109648080+ssssigun@users.noreply.github.com>
2024-07-31 21:50:35 +0900

    [level 3] Title: 단어 변환, Time: 0.49 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,55 +1,42 @@
 /*
-    1. bfs 변형으로 풀 수 있을 것 같다.
-        - 최단 거리 구하기
-        - 큐에 넣고 방문 처리
-        - 큐에서 하나씩 꺼내서 문자 하나만 다르면 큐에 저장
-        - 방문 처리, 거리 +1
+    1. dfs로 풀어보기
+        - 만약 도착지에 도착하면 반환
+        - 그게 아니라면 배열 순회
+        - 한글자만 다른 단어를 발견하면 방문 처리 후 dfs
 */
-import java.util.*;
-class Word{
-    String w;
-    int dis;
-    Word(String w, int dis){
-        this.w = w;
-        this.dis = dis;
-    }
-}
-
 class Solution {
+    int ans = Integer.MAX_VALUE;
+    String[] w;
     boolean[] visited;
     public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length];
-        return bfs(begin, target, words);
+        w = words.clone();
+        visited = new boolean[w.length];
+        dfs(begin, target, 0);
+        return ans == Integer.MAX_VALUE ? 0 : ans; // 값이 변한 적 없으면 도달x
     }
-    
-    // bfs
-    public int bfs(String begin, String target, String[] words){
-        Queue<Word> que = new LinkedList<>();
-        que.offer(new Word(begin, 0)); // 초기값
-        while(!que.isEmpty()){
-            Word w = que.poll();
-            if(target.equals(w.w)){ // 만약 타겟에 도착하면 거리값 반환
-                return w.dis;
-            }
-            for(int i=0; i<words.length; i++){ // 배열 순회
-                if(!visited[i] && ch(w.w, words[i])){ // 한글자 차이나면 방문x
-                    visited[i] = true;
-                    que.offer(new Word(words[i], w.dis + 1));
-                }
+    // dfs 함수
+    public void dfs(String begin, String target, int depth){
+        if(target.equals(begin)){ // target 단어에 도착했다면 최솟값 저장
+            ans = Math.min(ans, depth);
+            return;
+        }
+        for(int i=0; i<w.length; i++){ // 배열 순회
+            if(!visited[i] && ch(begin, w[i])){ // 방문 x, 알파벳 하나 차이일 때
+                visited[i] = true;
+                dfs(w[i], target, depth+1);
+                visited[i] = false;
             }
         }
-        return 0;
     }
-    
-    // 한글자만 다른지 확인 함수
+    // 알파벳 하나 차이인지 확인하는 함수
     public boolean ch(String str1, String str2){
         int cnt = 0;
         for(int i=0; i<str1.length(); i++){
-            if(str1.charAt(i) == str2.charAt(i)){
+            if(str1.charAt(i) != str2.charAt(i)){
                 cnt++;
             }
         }
-        if(str1.length() - cnt == 1){
+        if(cnt == 1){
             return true;
         }
         return false;
